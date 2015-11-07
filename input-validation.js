a7c14221727071fff9a31eb63a39783f8430f656
RecursiveLogic <qrx9823@gmail.com>
2015-11-07 15:05:41 -0500

    Added data parsing functions, still in progress

--- input-validation.js
+++ input-validation.js
@@ -1,7 +1,6 @@
 module.exports = function(input) {
     var symbols = input.split(' ');
     var filtered = symbols.map(x => x.match(/[a-zA-Z]/g));  
-    var stocks = filtered.map(x => x == null || x.length > 5 
+    return filtered.map(x => x == null || x.length > 5 
         ? '' : x.join(''));
-    return stocks;
 }
