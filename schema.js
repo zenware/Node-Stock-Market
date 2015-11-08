f63dcf2bc76681aa46652b5cbe63580a06c093a8
RecursiveLogic <qrx9823@gmail.com>
2015-11-08 12:51:50 -0500

    Refacted schema.js

--- schema.js
+++ schema.js
@@ -11,22 +11,30 @@ module.exports = {
             nullable: false,
             unique: true
         }
-    },
-    portfolios: {
-        id: {
-            type: 'increments',
-            nullable: false,
-            primary: true
-        },
-        name: {
-            type: 'string',
-            maxlength: 50,
-            nullable: false
-        },
-        stocks: {
-            type: 'string',
-            maxlength 50,
-            nullable: false
+        portfolios: {
+            id: {
+                type: 'increments',
+                nullable: false,
+                primary: true,
+                unique: true
+            },
+            name: {
+                type: 'string',
+                maxlength: 50,
+                nullable: false
+            },
+            stocks: {
+                id: {
+                    type: 'increments',
+                    nullable: false,
+                    primary: true
+                },
+                name: {
+                    type: 'string',
+                    maxlength: 20,
+                    nullable: false
+                }
+            }
         }
     }
 }
