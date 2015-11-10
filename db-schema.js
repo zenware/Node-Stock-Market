432aff0b5e101dd47cb62a255cab0bb9f360111a
robotmayo <robotmayo@gmail.com>
2015-11-09 23:24:08 -0500

    schemaaaaaaaaaaaaaaaaaaA

--- db-schema.js
+++ db-schema.js
@@ -1,53 +1,92 @@
-module.exports = {
-    users: {
-        id: {
-            type: 'increments',
-            nullable: false,
-            primary: true
-        },
-        email: {
-            type: 'string',
-            maxlength: 254,
-            nullable: false,
-            unique: true
+const drop = (knex, tbl) => knex.schema.dropTableIfExists(tbl);
+
+const schemas = {
+    users : function(knex){
+        const create = table => {
+            table
+            .increments('id')
+            .primary();
+            
+            table
+            .string('email')
+            .unique()
+            .notNullable();
+            
+            table
+            .string('password', 53)
+            .notNullable();
         }
+        return drop(knex, 'users')
+        .then(() => knex.schema.createTable('users', create));
     },
-    portfolio: {
-        id: {
-            type: 'increments',
-            nullable: false,
-            primary: true,
-            unique: true
-        },
-        name: {
-            type: 'string',
-            maxlength: 50,
-            nullable: false
+    portfolio : function(knex){
+        const create = table => {
+            table
+            .increments('id')
+            .primary();
+            
+            table
+            .string('name', 50)
+            .unique()
+            .notNullable();
         }
+        return drop(knex, 'portfolio')
+        .then(() => knex.schema.createTable('portfolio', create));
+        
     },
-    stock: {
-        company: {
-            type: 'string',
-            maxlength: 30,
-            nullable: false
-        },
-        exchange: {
-            type: 'string',
-            maxlength: 30,
-            nullable: true
-        },
-        symbol: {
-            type: 'string',
-            maxlength: 10,
-            nullable: false,
-            primary: true
+    stocks : function(knex){
+        const create = table => {
+             table
+            .increments('id')
+            .primary()
+            .notNullable();
+            
+            table
+            .string('company', 30)
+            .index()
+            .notNullable();
+            
+            
+            table
+            .string('exchange', 30)
+            .notNullable();
+            
+            table
+            .string('symbol', 10)
+            .index()
+            .notNullable();
+            
+            table
+            .unique(['exchange', 'symbol']);
         }
+        return drop(knex, 'stocks')
+        .then(() => knex.schema.createTable('stocks', create));
     },
-    stock_price: {
-        nullable: false,
-        price: { type: 'decimal' },
-        stock_id: true,
-        timestamp: 'timestamp',
-        type: 'increments'
+    stock_history : function(knex){
+        const create = table => {
+            table
+            .decimal('price')
+            .notNullable()
+            .index();
+            
+            table
+            .integer('id')
+            .index()
+            .references('id')
+            .inTable('stocks');
+        }
+        
+        return drop(knex, 'stock_history')
+        .then(() => knex.schema.createTable('stock_history', create));
+        
     }
+}
+
+module.exports = schemas;
+
+if(require.main === module){
+    console.log('Creating tables');
+    const config = require('./config.json');
+    const knex = require('knex')(config.knex);
+    Object.keys(schemas).map(k => schemas[k](knex));
 }
