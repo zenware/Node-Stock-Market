99c06ee70bfde62251ad4fa027bbb8e965dbd10d
RecursiveLogic <qrx9823@gmail.com>
2015-11-10 20:05:46 -0500

    Update

--- input-validation.js
+++ input-validation.js
@@ -1,6 +1,6 @@
 module.exports = function(input) {
     var symbols = input.split(' ');
     var filtered = symbols.map(x => x.match(/[a-zA-Z]/g));  
-    return filtered.map(x => x == null || x.length > 5 
+    return filtered.map(x => x == null || x.length > 6 
         ? '' : x.join(''));
 }
