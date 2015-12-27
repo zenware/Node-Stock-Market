33e111efba818468eaaf50535ba53740b8ca5da3
Jay Looney <jay.m.looney@gmail.com>
2015-12-26 20:36:02 -0500

    Foreign Keys didn't need to be explicitly declared.
    The primary key defaults to the reference if none is specified. I forgot
    about this at first.

--- schema/portfolios_table.sql
+++ schema/portfolios_table.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE "portfolios" (
 	"id" integer PRIMARY KEY,
-	"user_id" integer references users("id"),
+	"user_id" integer references users,
 	"name" text NOT NULL,
 	"created_date" timestamp with time zone,
 	"updated_date" timestamp with time zone	UPDATED_DATE,
--- schema/stock_transactions_table.sql
+++ schema/stock_transactions_table.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE "stock_transactions" (
 	id integer PRIMARY KEY,
-	portfolio_id integer references portfolios("id"),
+	portfolio_id integer references portfolios,
 	"buy_date" timestamp with time zone,
 	"sell_date" timestamp with time zone,
 	"buy_price" money,
